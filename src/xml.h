8a34dc6bac6b10d11c4df7d996237a98738f87db
Meng Lin <menglinmaker@gmail.com>
2024-06-22 01:38:49 +1000

    Chore: Shorten Licence

--- src/xml.h
+++ src/xml.h
@@ -8,18 +8,7 @@
 //  License as published by the Free Software Foundation; either
 //  version 2 of the License, or (at your option) any later version.
 //
-//  In addition, as a special exception, licensor gives permission to
-//  link the code of this work with the OpenSSL Library (or with modified
-//  versions of OpenSSL that use the same license as OpenSSL), and
-//  distribute linked combinations including the two. You must obey the
-//  GNU General Public License in all respects for all of the code used
-//  other than OpenSSL. If you modify this file, you may extend this
-//  exception to your version of the file, but you are not obligated to
-//  do so. If you do not wish to do so, delete this exception statement
-//  from your version. (This exception is necessary should this work be
-//  included in a GPL-licenced work.)
-//
-//  See COPYING.LIB for the licence text and disclaimer of warranty.
+//  See LICENCE for the licence text and disclaimer of warranty.
 //=============================================================================
 
 #ifndef __XML_H__

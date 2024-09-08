4f6f3cbafe05e713b10025a7f49e2e05568b763e
MengLinMaker <menglinmaker@gmail.com>
2024-09-09 01:48:45 +1000

    chore: remove licence headers

--- src/sfont.cpp
+++ src/sfont.cpp
@@ -1,16 +1,3 @@
-//=============================================================================
-//  MuseScore sftools
-//
-//  Copyright (C) 2004-2011 Werner Schweer
-//
-//  This work is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Library General Public
-//  License as published by the Free Software Foundation; either
-//  version 2 of the License, or (at your option) any later version.
-//
-//  See LICENCE for the licence text and disclaimer of warranty.
-//=============================================================================
-
 #include "sfont.h"
 #include <sndfile.h>
 #include <vorbis/vorbisenc.h>
